3d608f7171c6a61183adccc096f7e5b6ca068d25
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-06-26 01:12:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  var x = 20
+  var y = 10
+  var z = x + y
+
+  // console.log(z)
+
+  return (
+    <div>
+      <h1>An{z - 5}and</h1>
+      <p style={{color:"blue",backgroundColor:"cyan"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, repellendus.</p>
+    </div>
+  )
+}
+
+export default App;
